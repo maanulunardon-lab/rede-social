fe242f72b73ea5e3175eff5859dbc0eb080ebea8
maanulunardon-lab <maanu.lunardon@gmail.com>
2026-09-09 11:05:34 -0300

    ok

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const LikeBtn = document.querySelector(" . like-btn")
+    const postMedia =document.querySelector(".post-media");
+    if (!LikeBtn) return;
+
+    const likesCountSpan= likeBtn.querySelector(".likes-count");
+const bookmarkBtn = document.querySelector("bookmark-btn");
+
+let isLiked = false;
+let baseLikes = 0;
+
+
+})
